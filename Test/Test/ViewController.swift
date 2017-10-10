ca9ab29032d574983250ab8ef3a4d9c988f7d631
AbiSayuti <32661598+AbiSayuti@users.noreply.github.com>
2017-10-10 10:42:04 +0700

    test first repository

---
+++ Test/Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Abi Sayuti on 10/10/17.
+//  Copyright © 2017 Abi Sayuti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
